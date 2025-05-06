a7bdff914f160f16c722842846225402899ced36
joshua <joshuadsouza500@gmail.com>
2025-05-06 22:01:08 +0300

    Adding comments to code

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -14,8 +14,8 @@ function App() {
       <Suspense fallback={<Loader />}>
         <Routes>
           <Route path="/signup" element={<SignUp />} />
-          <Route path="/user/*" element={<User />} />
           <Route path="/signin" element={<SignIn />} />
+          <Route path="/user/*" element={<User />} />
           <Route path="/*" element={<CustomerRoutes />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
@@ -34,6 +34,7 @@ export default App;
    * Add Property add ability to add bullet points bold etc to description bar Sanitize , react quill {Sanitize in frontend before sending to backend just for security}
    * Write comments for the code
    * Add Meta Data
+   * Better to host backend not on vercel maybe render , railway because vercel is good for serverless data and socket io etc need constant connection hich vercel is not ideal for
    * 
    * EXTRAS:
    * For the project need to make the story match. If it is being created as a new modern verions then cant have yearsvof expiereicne etc maybe something else instead of it
